8adca9dcf84693d8c88f89dd3d6a221498dd7020
Irpan Padillah <irpan@argapro.id>
2024-11-10 10:25:09 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Test</p>
+        <button>Click Here</button>
+        <Button variant="ghost">Click Here</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
