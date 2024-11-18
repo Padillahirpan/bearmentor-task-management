6689784547dca37558a25cd008b469326ef0dfd1
Irpan Padillah <irpan@argapro.id>
2024-11-18 20:46:11 +0700

    feat: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import "./App.css";
 import { ThemeProvider } from "./components/theme-provider";
 import { NavBar } from "./components/home/header";
-import { ListGoalByStatus } from "./components/home/list-goal-by-status";
-import { DetailGoal } from "./components/detail/detail-goal";
-import { getGoalsData } from "./cons/dummydata";
-import { AddGoal } from "./pages/create-goal";
 
 function App() {
   return (
@@ -12,9 +8,6 @@ function App() {
       <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
         <div>
           <NavBar />
-          {/* <ListGoalByStatus /> */}
-          {/* <DetailGoal goalData={getGoalsData()[0]} /> */}
-          <AddGoal />
         </div>
       </ThemeProvider>
     </>
